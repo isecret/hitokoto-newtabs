82adc4a067e039e79a0853dc9721f2af3f6d6e93
Secret <i@wangmao.me>
2018-08-10 15:07:48 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,29 @@
+## 一言 for Chrome
+
+Chrome 新标签页，基于一言 _Hitokoto.cn_，简洁的黑白设计，基于 _animate.css_ 的过渡动画。
+
+![](https://static.openapi.link/img/WX20180810-130405.png)
+
+## 功能
+
+- [x] 点赞
+- [x] 更新
+- [ ] 自动切换
+- [ ] 设置后台
+- [ ] 搜索输入框
+
+## 依赖
+
+- [jquery](https://github.com/jquery/jquery)
+- [animate.css](https://github.com/daneden/animate.css)
+- [fetch](https://github.com/github/fetch)
+- [sweetalert](https://github.com/t4t5/sweetalert)
+- [iconfont](http://iconfont.cn/)
+
+## 说明
+
+项目下载后解压 Chrome 开启开发者模式并加载解压后的插件即可使用。如果你有更好的意见欢迎提交 issues 或者直接联系我~
+
+~~有时间~~（等有钱了）我会打包上架到 Chrome 网上应用。
+
+欢迎加入一言，QQ 群：70029304
---
+++ src/js/main.js
@@ -0,0 +1,90 @@
+$(function() {
+    var sid = 0;
+
+    // 获取一言
+    function hitokoto() {
+        try {
+            fetch("https://v1.hitokoto.cn/?encode=json", {
+                    headers:{
+                         'content-type':'application/json'
+                    }
+                }).then(function(response){
+                    return response.json();
+                }).then(function(data){
+                    sid = data.id;
+                    $(".title").animateCss("fadeIn");
+                    $(".hitokoto").text(data.hitokoto);
+                    $(".from").text("- 「" + data.from + '」');
+                });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    // 喜欢
+    function like(ID) {
+        fetch("https://hitokoto.cn/Like?ID=" + ID)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                swal(data.message, { button: "好的" });
+            });
+    }
+
+    // Animate
+    $.fn.extend({
+        animateCss: function (animationName, callback) {
+            var animationEnd = (function (el) {
+                var animations = {
+                    animation: 'animationend',
+                    OAnimation: 'oAnimationEnd',
+                    MozAnimation: 'mozAnimationEnd',
+                    WebkitAnimation: 'webkitAnimationEnd',
+                };
+
+                for (var t in animations) {
+                    if (el.style[t] !== undefined) {
+                        return animations[t];
+                    }
+                }
+            })(document.createElement('div'));
+
+            this.addClass('animated ' + animationName).one(animationEnd, function () {
+                $(this).removeClass('animated ' + animationName);
+
+                if (typeof callback === 'function') callback();
+            });
+
+            return this;
+        },
+    });
+
+    // Like 动画
+    function tadaHeart() {
+        $('.like').animateCss('tada');
+    }
+
+    // 旋转刷新
+    function rotateOutRefresh() {
+        $('.refresh').animateCss('anim-rotate');
+    }
+
+    // 循环播放动画
+    var tada = window.setInterval(tadaHeart, 600);
+
+    $(".like").click(function () {
+        like(sid);
+    });
+
+    $(".refresh").click(function () {
+        rotateOutRefresh();
+        hitokoto();
+    });
+
+    $(".about").click(function () {
+        swal({text: "基于一言 Hitokoto.cn，插件由 Secret 开发维护~\nQQ 群：70029304，欢迎你的加入~⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄", button: "好的", title: '关于一言' });
+    });
+
+    hitokoto();
+});
